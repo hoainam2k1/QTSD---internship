6cc2a785f87e738e4e5990c6a8c29ac852773277
hoainam2k1 <hoainam20052001@gmail.com>
2022-08-20 17:38:42 +0700

    commit OOP

---
+++ QTSD-internship/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTSD_internship
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+        public Person()
+        {
+
+        }
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Name: {0} \nAge: {1} \nAddress: {2}", Name, Age, Address);
+        }
+        public void Create()
+        {
+            Console.WriteLine("Name: ?");
+            Console.ReadLine();
+            Console.WriteLine("Age: ?");
+            Console.ReadLine();
+            Console.WriteLine("Address: ?");
+            Console.ReadLine();
+        }
+    }
+}
