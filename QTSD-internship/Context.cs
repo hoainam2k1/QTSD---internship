e0b87d3c86831bedfbb96808f83e2e599b2b5dd4
hoainam2k1 <hoainam20052001@gmail.com>
2022-08-21 19:36:40 +0700

    commit IEnumrable vs IQueryable

---
+++ QTSD-internship/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace QTSD_internship
+{
+    public class Context : DbContext
+    {
+        private readonly string connectionString;
+        private readonly ILoggerFactory loggerFactory = LoggerFactory.Create(configure => configure.AddConsole());
+        public Context(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public DbSet<Employees> employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ QTSD-internship/Employees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTSD_internship
+{
+    [Table("Employees")]
+    public class Employees
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
